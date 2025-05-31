c00d7d9a05de464a1819e8936b8b058456f33b1b
Lora <130064896+Lora-cdm@users.noreply.github.com>
2025-05-31 05:25:55 -0700

    Calculator ish

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-var x = 5;
 
-if(x>2){
+var x = 5;
+if (x > 2) {
     console.log("X eshte me i madh se 2");
-}else{
+} else {
     console.log("X eshte me i vogel se 2");
 }
 
 var y = 1;
-
-if(y>2){
+if (y > 2) {
     console.log("Y eshte me i madh se 2");
-}else{
+} else {
     console.log("Y eshte me i vogel se 2");
 }
 
@@ -18,11 +17,15 @@ var input = document.getElementById('inputid');
 var btn = document.getElementById('btnid');
 var text = document.getElementById('textid');
 
-btn.onclick=function(){
-    text.innerHTML=input.value;
+btn.onclick = function() {
+    text.innerHTML = input.value;
 }
 
-var input2 = document.querySelector('#input2')
-var input3 = document.querySelector('#input3')
-var btn2 = document.querySelector('#btn2id')
-var answer = document.querySelector('#answer')
+var input2 = document.querySelector('#input2');
+var input3 = document.querySelector('#input3'); 
+var btn2 = document.querySelector('#btn2id');
+var answer = document.querySelector('#answer');
+
+btn2.addEventListener("click", function() {
+    answer.innerHTML = parseInt(input2.value) + parseInt(input3.value); 
+});
