0b34f68120470d2e82c02ea311eebe4a5d0cfe11
Lora <130064896+Lora-cdm@users.noreply.github.com>
2025-05-31 04:31:41 -0700

    If else

---
+++ index.js
@@ -0,0 +1,15 @@
+var x = 5;
+
+if(x>2){
+    console.log("X eshte me i madh se 2")
+}else{
+    console.log("X eshte me i vogel se 2")
+}
+
+var y = 1;
+
+if(y>2){
+    console.log("Y eshte me i madh se 2")
+}else{
+    console.log("Y eshte me i vogel se 2")
+}
