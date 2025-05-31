546fe84b15d91301d724fdf659ec053b1b3adc3c
Lora <130064896+Lora-cdm@users.noreply.github.com>
2025-05-31 05:06:45 -0700

    edits

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 var x = 5;
 
 if(x>2){
-    console.log("X eshte me i madh se 2")
+    console.log("X eshte me i madh se 2");
 }else{
-    console.log("X eshte me i vogel se 2")
+    console.log("X eshte me i vogel se 2");
 }
 
 var y = 1;
 
 if(y>2){
-    console.log("Y eshte me i madh se 2")
+    console.log("Y eshte me i madh se 2");
 }else{
-    console.log("Y eshte me i vogel se 2")
+    console.log("Y eshte me i vogel se 2");
 }
 
-var input = document.getElementById('inputid')
-var btn = document.getElementById('btnid')
-var text = document.getElementById('textid')
+var input = document.getElementById('inputid');
+var btn = document.getElementById('btnid');
+var text = document.getElementById('textid');
 
-button.onclick=function(){
-    text.innerHTML=input.value
+btn.onclick=function(){
+    text.innerHTML=input.value;
 }
